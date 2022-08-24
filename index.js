28b858e20ebeee460b6c0bb95b4619c24c558009
mihir hadavani <miheerhadvani990@gmail.com>
2022-08-24 21:03:21 +0530

    update

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const express = require('express')
 const app = express();
 
 const port = 4552;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Server is up and running"
+    })
+})
+
+
+app.get("*", (req, res) => {
+    res.status(404).json({
+        message: "this route does not exist"
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`);
+})
