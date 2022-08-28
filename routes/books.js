9cef64a3fd3ee7ee9f2a2fefdaacaf85191b6795
mihir hadavani <miheerhadvani990@gmail.com>
2022-08-28 10:48:02 +0530

    update

--- routes/books.js
+++ routes/books.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 const { books } = require('../data/books.json');
 const { users } = require('../data/users.json');
@@ -97,7 +98,80 @@ router.get('/:id', (req, res) => {
     })
 })
 
+/*
+route books/
+method post
+parameters : none
+add a new book
+*/
 
+
+
+router.post('/', (req, res) => {
+    const data = req.body;
+
+    if (!data) {
+        return res.status(400).json({
+            sucess: false,
+            message: "No data provided"
+        })
+    }
+
+    const book = books.find((each) => each.id === data.id);
+
+
+    if (book) {
+        return res.status(404).json({
+            sucess: false,
+            message: "book is already exist with this id"
+        })
+    }
+
+
+    const allbooks = [...books, data];
+    return res.status(201).json({
+        sucess: true,
+        message: "book added",
+        data: allbooks
+    })
+})
+
+
+
+
+/**
+ * Route: /books/:id
+ * Method: PUT
+ * Description: Update book
+ * Access: Public
+ * Parameters: id
+ * Data: author, name, genre, price, publisher, id
+ */
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+
+    const book = books.find((each) => each.id === id);
+
+    if (!book) {
+        return res.status(400).json({
+            success: false,
+            message: "Book not found with this particular id",
+        });
+    }
+
+    const updateData = books.map((each) => {
+        if (each.id === id) {
+            return {...each, ...data };
+        }
+        return each;
+    });
+
+    return res.status(200).json({
+        success: true,
+        data: updateData,
+    });
+});
 
 
 
